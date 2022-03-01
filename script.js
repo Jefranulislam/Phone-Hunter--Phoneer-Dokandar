35e259e35f78075c7f6433cbc39549f15351ef7c
JefranulIslam <jefranulislam@gmail.com>
2022-03-01 20:37:40 +0600

    lot of things are fixed

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const searchButton = () => {
     const searchFiled = document.getElementById('searchFiled');
     const searchFiledText = searchFiled.value;
-    console.log(searchFiledText);
+    // console.log(searchFiledText);
 
     searchFiled.value = '';
     const url = `https://openapi.programming-hero.com/api/phones?search=${searchFiledText}`
@@ -14,21 +14,49 @@ const searchButton = () => {
 const displaySearchResult = data => {
     const searchResultCol = document.getElementById('searchResultCol');
     data.forEach(data => {
-        console.log(data);
+        // console.log(data);
         const div = document.createElement('div');
         div.classList.add('col');
         div.innerHTML = ` <div class="card h-100">
-        <img src="${data.image}" class="card-img-top" alt="...">
+        <img src="${data.image}" class="card-img-top w-75 mx-auto p-4" alt="...">
         <div class="card-body">
             <h5 class="card-title">${data.phone_name}</h5>
-            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to
-                additional content. This content is a little bit longer.</p>
+            <p class="card-text">Brand: ${data.brand}</p>
         </div>
-        <div class="card-footer d-grid gap-2">
-        <button class="btn btn-primary" type="button">Learn More</button>
+        <div class="card-footer d-grid gap-2 alert-primary ">
+        <button onclick="phoneDetilesId('${data.slug}')" class="btn btn-primary" type="button">Learn More</button>
         </div>
     </div>`
         searchResultCol.appendChild(div);
 
     });
+}
+
+
+// Phone Detiels 
+const phoneDetilesId = slugs => {
+    const slugurl = `https://openapi.programming-hero.com/api/phone/${slugs}`
+
+    fetch(slugurl)
+        .then(res => res.json())
+        .then(data => console.log(data));
+}
+
+const displayDetiles = phoneInfo => {
+    const phonedetils = document.getElementById('phoneDisplayDetiles');
+    const div = document.createElement('div');
+    div.classList.add('card');
+    div.innerHTML = `<div class="row g-0 alert-primary">
+     <div class="col-md-4">
+         <img src="${phoneInfo.data.image}" class="img-fluid rounded-start " alt="...">
+     </div>
+     <div class="col-md-8">
+         <div class="card-body">
+             <h5 class="card-title">${phoneInfo.data.name}</h5>
+             <p class="card-text">${phoneInfo.data.releaseDate}</p>
+             <p class="card-text">${phoneInfo.data.main}</p>
+         </div>
+     </div>
+ </div>`
+    phonedetils.appendChild(div);
 }
